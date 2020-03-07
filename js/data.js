99ec4b812cb34dbeb7d632a33194ce02ae9c6f98
Joakim Algrøy <joakimalgroy@gmail.com>
2020-03-07 18:42:03 +0100

    Add date label, title, table over fylker

--- js/data.js
+++ js/data.js
@@ -1,132 +1,146 @@
-var data = {
-    0: {
-        "03": 0,
-        "11": 0,
-        "15": 0,
-        "18": 0,
-        "30": 0,
-        "34": 0,
-        "38": 0,
-        "42": 0,
-        "46": 0,
-        "50": 0,
-        "54": 1,
+// Day zero = 2020-02-26
+var data = [
+    {
+        "03": 0, // Oslo
+        "11": 0, // Rogaland
+        "15": 0, // Møre og Romsdal
+        "18": 0, // Nordland
+        "30": 0, // Viken
+        "34": 0, // Innlandet
+        "38": 0, // Vestfold og Telemark
+        "42": 0, // Agder
+        "46": 0, // Vestland
+        "50": 0, // Trøndelag
+        "54": 1, // Troms og Finnmark
     },
-    1: {
-        "03": 0,
-        "11": 0,
-        "15": 0,
-        "18": 0,
-        "30": 0,
-        "34": 0,
-        "38": 0,
-        "42": 0,
-        "46": 0,
-        "50": 0,
-        "54": 1,
+    {
+        "03": 2, // Oslo
+        "11": 0, // Rogaland
+        "15": 0, // Møre og Romsdal
+        "18": 0, // Nordland
+        "30": 1, // Viken
+        "34": 0, // Innlandet
+        "38": 0, // Vestfold og Telemark
+        "42": 0, // Agder
+        "46": 0, // Vestland
+        "50": 0, // Trøndelag
+        "54": 1, // Troms og Finnmark
     },
-    2: {
-        "03": 0,
-        "11": 0,
-        "15": 0,
-        "18": 0,
-        "30": 0,
-        "34": 0,
-        "38": 0,
-        "42": 0,
-        "46": 0,
-        "50": 0,
-        "54": 1,
+    {
+        "03": 3, // Oslo
+        "11": 0, // Rogaland
+        "15": 0, // Møre og Romsdal
+        "18": 0, // Nordland
+        "30": 1, // Viken
+        "34": 0, // Innlandet
+        "38": 0, // Vestfold og Telemark
+        "42": 0, // Agder
+        "46": 1, // Vestland
+        "50": 0, // Trøndelag
+        "54": 1, // Troms og Finnmark
     },
-    3: {
-        "03": 0,
-        "11": 0,
-        "15": 0,
-        "18": 0,
-        "30": 0,
-        "34": 0,
-        "38": 0,
-        "42": 0,
-        "46": 0,
-        "50": 0,
-        "54": 1,
+    {
+        "03": 6, // Oslo
+        "11": 0, // Rogaland
+        "15": 0, // Møre og Romsdal
+        "18": 0, // Nordland
+        "30": 3, // Viken
+        "34": 0, // Innlandet
+        "38": 0, // Vestfold og Telemark
+        "42": 0, // Agder
+        "46": 5, // Vestland
+        "50": 0, // Trøndelag
+        "54": 1, // Troms og Finnmark
     },
-    4: {
-        "03": 0,
-        "11": 0,
-        "15": 0,
-        "18": 0,
-        "30": 0,
-        "34": 0,
-        "38": 0,
-        "42": 0,
-        "46": 0,
-        "50": 0,
-        "54": 1,
+    {
+        "03": 7, // Oslo
+        "11": 0, // Rogaland
+        "15": 0, // Møre og Romsdal
+        "18": 0, // Nordland
+        "30": 5, // Viken
+        "34": 0, // Innlandet
+        "38": 0, // Vestfold og Telemark
+        "42": 0, // Agder
+        "46": 6, // Vestland
+        "50": 0, // Trøndelag
+        "54": 1, // Troms og Finnmark
     },
-    5: {
-        "03": 0,
-        "11": 0,
-        "15": 0,
-        "18": 0,
-        "30": 0,
-        "34": 0,
-        "38": 0,
-        "42": 0,
-        "46": 0,
-        "50": 0,
-        "54": 1,
+    {
+        "03": 7, // Oslo
+        "11": 1, // Rogaland
+        "15": 0, // Møre og Romsdal
+        "18": 0, // Nordland
+        "30": 5, // Viken
+        "34": 0, // Innlandet
+        "38": 0, // Vestfold og Telemark
+        "42": 2, // Agder
+        "46": 9, // Vestland
+        "50": 0, // Trøndelag
+        "54": 1, // Troms og Finnmark
     },
-    6: {
-        "03": 0,
-        "11": 0,
-        "15": 0,
-        "18": 0,
-        "30": 0,
-        "34": 0,
-        "38": 0,
-        "42": 0,
-        "46": 0,
-        "50": 0,
-        "54": 1,
+    {
+        "03": 7, // Oslo
+        "11": 1, // Rogaland
+        "15": 0, // Møre og Romsdal
+        "18": 0, // Nordland
+        "30": 8, // Viken
+        "34": 0, // Innlandet
+        "38": 0, // Vestfold og Telemark
+        "42": 3, // Agder
+        "46": 12, // Vestland
+        "50": 0, // Trøndelag
+        "54": 2, // Troms og Finnmark
     },
-    7: {
-        "03": 0,
-        "11": 0,
-        "15": 0,
-        "18": 0,
-        "30": 0,
-        "34": 0,
-        "38": 0,
-        "42": 0,
-        "46": 0,
-        "50": 0,
-        "54": 1,
+    {
+        "03": 11, // Oslo
+        "11": 3, // Rogaland
+        "15": 1, // Møre og Romsdal
+        "18": 0, // Nordland
+        "30": 15, // Viken
+        "34": 3, // Innlandet
+        "38": 0, // Vestfold og Telemark
+        "42": 3, // Agder
+        "46": 17, // Vestland
+        "50": 1, // Trøndelag
+        "54": 2, // Troms og Finnmark
     },
-    8: {
-        "03": 0,
-        "11": 0,
-        "15": 0,
-        "18": 0,
-        "30": 0,
-        "34": 0,
-        "38": 0,
-        "42": 0,
-        "46": 0,
-        "50": 0,
-        "54": 1,
+    {
+        "03": 18, // Oslo
+        "11": 3, // Rogaland
+        "15": 2, // Møre og Romsdal
+        "18": 0, // Nordland
+        "30": 21, // Viken
+        "34": 4, // Innlandet
+        "38": 2, // Vestfold og Telemark
+        "42": 5, // Agder
+        "46": 22, // Vestland
+        "50": 7, // Trøndelag
+        "54": 2, // Troms og Finnmark
     },
-    9: {
-        "03": 0,
-        "11": 0,
-        "15": 0,
-        "18": 0,
-        "30": 0,
-        "34": 0,
-        "38": 0,
-        "42": 0,
-        "46": 0,
-        "50": 0,
-        "54": 1,
-    }
-}
+    {
+        "03": 23, // Oslo
+        "11": 10, // Rogaland
+        "15": 2, // Møre og Romsdal
+        "18": 0, // Nordland
+        "30": 25, // Viken
+        "34": 6, // Innlandet
+        "38": 2, // Vestfold og Telemark
+        "42": 5, // Agder
+        "46": 23, // Vestland
+        "50": 11, // Trøndelag
+        "54": 6, // Troms og Finnmark
+    },
+    {
+        "03": 28, // Oslo
+        "11": 15, // Rogaland
+        "15": 2, // Møre og Romsdal
+        "18": 1, // Nordland
+        "30": 37, // Viken
+        "34": 12, // Innlandet
+        "38": 2, // Vestfold og Telemark
+        "42": 9, // Agder
+        "46": 24, // Vestland
+        "50": 11, // Trøndelag
+        "54": 6, // Troms og Finnmark
+    },
+]
--- js/javascript.js
+++ js/javascript.js
@@ -1,19 +1,155 @@
-for (let fn in data[0]) {
-    for(let i = 0; i < fylker["features"].length; i++){
-        if(fylker["features"][i]["properties"]["fylkesnummer"] == fn){
-            fylker["features"][i]["properties"]["infected"] = data[0][fn];
-            console.log(fylker["features"][i]["properties"]);
-        }
+Date.prototype.addDays = function(days) {
+        var date = new Date(this.valueOf());
+        date.setDate(date.getDate() + days);
+        return date;
+}
+
+const dateZero = new Date("2020-02-26");
+
+function fnToName(fn) {
+    let num = parseInt(fn);
+    switch (num) {
+        case 11: return "Rogaland";
+        case 38: return "Vestfold og Telemark";
+        case 18: return "Nordland";
+        case 42: return "Agder";
+        case 34: return "Innlandet";
+        case 15: return "Møre og Romsdal";
+        case 46: return "Vestland";
+        case 3: return "Oslo";
+        case 30: return "Viken";
+        case 50: return "Trøndelag";
+        case 54: return "Troms og Finnmark";
+        default: return "";
+    }
+
+}
+
+function fnToInd(fn){
+    let num = parseInt(fn);
+    switch (num) {
+        case 11: return 0;
+        case 38: return 1;
+        case 18: return 2;
+        case 42: return 3;
+        case 34: return 4;
+        case 15: return 5;
+        case 46: return 6;
+        case 3: return 7;
+        case 30: return 8;
+        case 50: return 9;
+        case 54: return 10;
+        default: return -1;
     }
 }
 
-console.log(fylker)
+function dateToString(date){
+    let months = ["Jan.","Feb.","Mars","Apr.","Mai","Juni","Juli","Aug.","Sep.","Okt.","Nov.","Des."];
+    return date.getDate() + ". " + months[date.getMonth()] + " " + date.getFullYear();
+}
 
-var map = L.map('map');
+function style(fylke) {
+    return { fillColor: getColor(fylke.properties.infected),
+        weight: 1,
+        opacity: 1,
+        color: 'black',
+        fillOpacity: 0.7
+    };
+}
 
-var positron = 
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png').addTo(map);
+function getColor(inf) {
+    return  inf > 25000 ? "#220000" :
+            inf > 10000 ? "#440000" :
+            inf > 5000  ? "#660000" :
+            inf > 2000  ? "#880000" :
+            inf > 1000  ? "#AA0000" :
+            inf > 500   ? "#CC0000" :
+            inf > 200   ? "#EE0000" :
+            inf > 100   ? "#FF1111" :
+            inf > 50    ? "#FF3333" :
+            inf > 25    ? "#FF5555" :
+            inf > 10    ? "#FF7777" :
+            inf > 5     ? "#FF9999" :
+            inf > 0     ? "#FFBBBB" :
+                          "#CCCCCC" ;
+}
 
-L.geoJson(fylker).addTo(map);
+var geojson;
 
-map.setView([62,5],4)
+function highlightFeature(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+        weight: 2,
+        fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+    }
+}
+
+function resetHighlight(e) {
+    geojson.resetStyle(e.target);
+}
+
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+    });
+}
+
+var c = 0;
+
+function setDay(d){
+    let tableHtml = "<table class='infotable'><tr><th>Fylke</th><th>Antall smittede</th></tr>";
+    let sum = 0;
+    for (let fn in data[d]) {
+        fylker["features"][fnToInd(fn)]["properties"]["infected"] = data[d][fn];
+        tableHtml += "<tr><td>";
+        tableHtml += fylker["features"][fnToInd(fn)]["properties"]["navn"];
+        tableHtml += "</td><td class='numinf'>" + data[d][fn] + "</td></tr>";
+        sum += data[d][fn];
+    }
+    tableHtml += "<tr><td><b>Totalt</b></td><td class='numinf'><b>" + sum + "</b></td></tr></table>"
+    if(map){
+        map.remove();
+    }
+    // Build map
+    map = L.map('map', { zoomControl: false, scrollWheelZoom: false, doubleClickZoom: false, dragging: false, touchZoom: false});
+    geojson = L.geoJson(fylker, {
+        style: style,
+        onEachFeature: onEachFeature
+    }).addTo(map);
+    map.fitBounds([[72,2],[55,30]])
+
+    // Add date
+    let date = dateToString(dateZero.addDays(d));
+    let dateMarker = new L.marker([66,1], { opacity: 0 });
+    dateMarker.bindTooltip(date, {permanent: true, className: "date-label", offset: [0, 0] });
+    dateMarker.addTo(map);
+
+    // Add data:
+    let infoMarker = new L.marker([62,15.5], { opacity: 0 }); 
+    infoMarker.bindTooltip(tableHtml, {permanent: true, className: "table-label", offset: [0, 0] });
+    infoMarker.addTo(map);
+}
+
+
+var map;
+
+setDay(0);
+
+var day = 0;
+var animation = setInterval(updateDay, 1000);
+
+function updateDay(){
+    day++;
+    if(day >= data.length){
+        clearInterval(animation);
+        return
+    }
+    setDay(day);
+    console.log(Date.now()/1000);
+}
